6797a6cce6c86cc7cce3042ee10cdb49b8357951
alex85amy <alex85amy@gmail.com>
2024-05-13 15:51:26 +0800

    update

--- src/main/java/com/example/training/test.java
+++ src/main/java/com/example/training/test.java
@@ -1,13 +1,6 @@
 package com.example.training;
 
-import com.example.training.util.ExportToCsv;
-
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Arrays;
-
 public class test {
-    public static void main(String[] args) throws SQLException, IOException {
-        ExportToCsv exportToCsv = new ExportToCsv();
+    public static void main(String[] args) {
     }
 }
